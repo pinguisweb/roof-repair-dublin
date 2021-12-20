404b1f646f1bcfcd49eb3180c6433a7798629d01
pinguisweb.com <pinguisweb@gmail.com>
2021-12-20 14:03:43 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <title>Roof Repair | Fixing Your Leaking Roof</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <Header title="Welcome to my Roofers Dublin!" />
         <p className="description">
@@ -17,7 +16,7 @@ export default function Home() {
      <p>  Our experts are highly trained and have long years of experience in the industry.  <p>  
     Roofers Dublin offer impeccable customer service and work.  <p>  
     We believe that the client knows best, and we will work hand in hand with you to ensure that your roof is constructed or repaired to your specifications, <p>  
-    while offering advice to make it even better.
+    we also offer advice to make it even better.
         </p>
     <p>  Roofers providing <a href="https://bit.ly/2R2LQmj">roofing services</a> if you need specialist quality roofing services, get a highly experienced team responsible></p>
       </main>
