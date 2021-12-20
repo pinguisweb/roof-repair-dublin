00d602f49c20d31179d8ec018e2a31dfb92f61fa
pinguisweb.com <pinguisweb@gmail.com>
2021-12-20 11:53:23 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Roof Repair | Fixing Your Leaking Roof | Get the Local Roofers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
