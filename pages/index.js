92ed3350e23cb2b774e8323943f2dc59994aa37b
pinguisweb.com <pinguisweb@gmail.com>
2021-12-20 14:13:34 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Roof Repair | Fixing Your Leaking Roof</title>
+        <title>Roof Repair | We Fix Your Leaking Roofs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <Header title="Welcome to my Roofers Dublin!" />
         <p className="description">
-          Local Professional We make use of the latest construction equipment and technology in our work.
-     <p>  Our experts are highly trained and have long years of experience in the industry.  <p>  
+          Local Professional we make use of the latest construction equipment and technology in our work.
+     <p>  Our experts are highly trained and have over 20 years of experience in the industry.  <p>  
     Roofers Dublin offer impeccable customer service and work.  <p>  
     We believe that the client knows best, and we will work hand in hand with you to ensure that your roof is constructed or repaired to your specifications, <p>  
     we also offer advice to make it even better.
