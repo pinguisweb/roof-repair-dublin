db4798e41e68fbe116cbe6097a12cf2216ed19c5
pinguisweb.com <pinguisweb@gmail.com>
2021-12-20 14:23:26 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,11 @@ export default function Home() {
           Local Professional we make use of the latest construction equipment and technology in our work.
      <p>  Our experts are highly trained and have over 20 years of experience in the industry.  <p>  
     Roofers Dublin offer impeccable customer service and work.  <p>  
-    We believe that the client knows best, and we will work hand in hand with you to ensure that your roof is constructed or repaired to your specifications, <p>  
+    We believe that the client knows best, and we will work hand in hand with you to ensure that your roof is constructed or repaired to your specifications, <p> 
     we also offer advice to make it even better.
         </p>
     <p>  Roofers providing <a href="https://bit.ly/2R2LQmj">roofing services</a> if you need specialist quality roofing services, 
-    get a highly experienced team></p>
+    get a highly experienced team in dublin.></p>
       </main>
 
       <Footer />
