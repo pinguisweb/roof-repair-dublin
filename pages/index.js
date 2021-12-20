0d8c3aea423bd47adb02ade1b63e45a54f7c8766
pinguisweb.com <pinguisweb@gmail.com>
2021-12-20 11:51:04 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,14 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Local Professional
+<br />
+We make use of the latest construction equipment and technology in our work. <br />
+    Our experts are highly trained and have long years of experience in the industry. <br />
+    Roofers Dublin offer impeccable customer service and work. <br />
+    We believe that the client knows best, and we will work hand in hand with you to ensure that your roof is constructed or repaired to your specifications while offering advice to make it even better.
         </p>
+    <p>  Roofers providing <a href="https://bit.ly/2R2LQmj">roofing services</a> if you need specialist quality roofing services, get a highly experienced team responsible></p>
       </main>
 
       <Footer />
